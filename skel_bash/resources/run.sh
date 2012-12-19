89ecae0c05924f16b8622d338b5dc8672783237b
George Asimenos <george+github@dnanexus.com>
2012-12-19 14:07:28 -0800

    Refreshing samtools merge, removing skel_bash

--- skel_bash/resources/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-PATH="/:${PATH}"
-HOME="`pwd`"
-
-# Example for retrieving file-123 from input: {"alignment": {"$dnanexus_link": "file-123"}}
-#jshon -e alignment -e '$dnanexus_link' -u < job_input.json
-
-echo '{}' > "$HOME/job_output.json"
